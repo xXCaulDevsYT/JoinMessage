0b4aae85dc7432fa2775ad66d0e354fa6b9b1561
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-10-31 13:54:14 -0500

    Update Main.php

--- src/sean/JoinMessage/Main.php
+++ src/sean/JoinMessage/Main.php
@@ -11,11 +11,19 @@ class Main extends PluginBase implements Listener{
 
      public function onEnable(){
      $this->getServer()->getPluginManager->registerEvents($this);
-     $this->getLogger()->info("JoinMessage enabled!");
+     $this->getLogger()->info("[EmeraldMC] Enabled AuroraWelcoming!");
      }
 
      public function onPlayerJoin(PlayerJoinEvent $event){
      $p = $event->getPlayer();
-     $p->sendMessage("Welcome to my server!");
+     $p->sendMessage("§b===============§3===============§r");
+     $p->sendMessage("§l         §dAurora§5HCF§r");
+     $p->sendMessage("§l§r§b§r");
+     $p->sendMessage("§l§a Store:§r§7 AuroraMCPE.buycraft.net");
+     $p->sendMessage("§l§a Vote:§r§7 Coming Soon§r");
+     $p->sendMessage("§l§a Discord:§r§7 Coming Soon§r");
+     $p->sendMessage("§l§a Website:§r§7 Coming Soon§r");
+     $p->sendMessage("§l§r§a§r");
+     $p->sendMessage("§3===============§b===============§r");
      }
 }
